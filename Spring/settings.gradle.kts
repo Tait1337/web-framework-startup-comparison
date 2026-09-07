db13667d9a2f96b5b521185e73aa38306a6d9aa2
Oliver <34075572+Tait1337@users.noreply.github.com>
2026-09-07 13:14:33 +0200

    Update settings.gradle.kts for repository management
    Added legacy repository for Spring Native and updated repository management.

--- Spring/settings.gradle.kts
+++ Spring/settings.gradle.kts
@@ -1,8 +1,27 @@
-rootProject.name = "spring"
-
 pluginManagement {
     repositories {
-        maven { url = uri("https://repo.spring.io/release") }
+        // Legacy repository required by Spring Native / Spring AOT 0.12.x.
+        maven {
+            url = uri("https://repo.spring.io/plugins-release")
+        }
+
+        maven {
+            url = uri("https://repo.spring.io/release")
+        }
+
         gradlePluginPortal()
+        mavenCentral()
     }
 }
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.spring.io/release")
+        }
+
+        mavenCentral()
+    }
+}
+
+rootProject.name = "spring"
