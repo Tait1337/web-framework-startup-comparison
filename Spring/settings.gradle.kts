cecdc9ddf6a1233343a1e03820dc4ae08f7767cc
Oliver <34075572+Tait1337@users.noreply.github.com>
2026-09-07 09:49:38 +0200

    Remove snapshot repository from plugin management

--- Spring/settings.gradle.kts
+++ Spring/settings.gradle.kts
@@ -1,8 +1,8 @@
 rootProject.name = "spring"
+
 pluginManagement {
     repositories {
         maven { url = uri("https://repo.spring.io/release") }
-        maven { url = uri("https://repo.spring.io/snapshot") }
         gradlePluginPortal()
     }
 }
